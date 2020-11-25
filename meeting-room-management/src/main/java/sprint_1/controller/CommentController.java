8c14071e1774a4a09908bf8de8a6681d80d3a28f
quangnguyen96 <quangnguyendn96@gmail.com>
2020-11-25 14:46:12 +0700

    Merge branch 'main' of https://github.com/Chuong-Hoang/C0520G1-Project-BackEnd into nguyenhuuquang
    # Conflicts:
    #	meeting-room-management/src/main/resources/application.properties

--- meeting-room-management/src/main/java/sprint_1/controller/CommentController.java
+++ meeting-room-management/src/main/java/sprint_1/controller/CommentController.java
@@ -1,5 +1,4 @@
 package sprint_1.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
