c54579261d116c28453227c612d2cb6b4eb342ba
nhatle352840 <leenhat11a1nh@gmail.com>
2020-11-25 19:40:32 +0700

    nhat commit

--- meeting-room-management/src/main/java/sprint_1/controller/CommentController.java
+++ meeting-room-management/src/main/java/sprint_1/controller/CommentController.java
@@ -18,10 +18,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * CommentController
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date: 24-11-2020
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE                 AUTHOR          DESCRIPTION
+ * -----------------------------------------------------------------------
+ * 24-11-2020         NhatL            Create
  */
 @RestController
 @CrossOrigin
+@RequestMapping()
 public class CommentController {
     @Autowired
     CommentService commentService;
@@ -36,61 +48,65 @@ public class CommentController {
     @GetMapping("/comment")
     public ResponseEntity<List<CommentDTO>> getListComment() {
         List<CommentDTO> commentDTO = new ArrayList<>();
-
         List<Comment> commentList = commentService.findAll();
-        if (commentList.isEmpty()) {
+        if (commentList == null) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
-            for (Comment commentLists : commentList) {
-                if (commentLists.getReplier() == null) {
-                    commentDTO.add(new CommentDTO(commentLists.getIdComment(), commentLists.getSender().getFullName(), commentLists.getCommentTime(), "N/A",
-                            commentLists.getContentComment(), "N/A", commentLists.getErrorType().getErrorTypeName(),
-                            commentLists.getMeetingRoom().getRoomName(), commentLists.isStatus(),commentLists.isStatusView()));
-                } else {
-                    commentDTO.add(new CommentDTO(commentLists.getIdComment(), commentLists.getSender().getFullName(),
-                            commentLists.getCommentTime(), commentLists.getContentReply(), commentLists.getContentComment(),
-                            commentLists.getReplier().getFullName(), commentLists.getErrorType().getErrorTypeName(),
-                            commentLists.getMeetingRoom().getRoomName(), commentLists.isStatus(),commentLists.isStatusView()));
+            if (commentList.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } else {
+                for (Comment commentLists : commentList) {
+                    if (commentLists.getReplier() == null) {
+                        commentDTO.add(new CommentDTO(commentLists.getIdComment(), commentLists.getSender().getFullName(), commentLists.getCommentTime(), "N/A",
+                                commentLists.getContentComment(), "N/A", commentLists.getErrorType().getErrorTypeName(),
+                                commentLists.getMeetingRoom().getRoomName(), commentLists.isStatus(), commentLists.isStatusView()));
+                    } else {
+                        commentDTO.add(new CommentDTO(commentLists.getIdComment(), commentLists.getSender().getFullName(),
+                                commentLists.getCommentTime(), commentLists.getContentReply(), commentLists.getContentComment(),
+                                commentLists.getReplier().getFullName(), commentLists.getErrorType().getErrorTypeName(),
+                                commentLists.getMeetingRoom().getRoomName(), commentLists.isStatus(), commentLists.isStatusView()));
 
+                    }
                 }
+                return new ResponseEntity<>(commentDTO, HttpStatus.OK);
             }
-            return new ResponseEntity<>(commentDTO, HttpStatus.OK);
         }
     }
-//admin
+
+    //admin
     @GetMapping("/comment/search")
     public ResponseEntity<List<CommentDTO>> findCommentByRoomName(@RequestParam("value1") String userNameSearch, @RequestParam("value2") String roomNameSearch, @RequestParam("value3") boolean statusSearch) {
         List<Comment> listAll = commentService.findAll();
-        List<Comment> commentListUserName ;
+        List<Comment> commentListUserName;
         if (listAll.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
 
 // (1) search by userName
-        if ("".equals(userNameSearch)){
+        if ("".equals(userNameSearch)) {
             commentListUserName = listAll;
         } else {
-           commentListUserName = commentService.findAllBySender(userNameSearch);
+            commentListUserName = commentService.findAllBySender(userNameSearch);
         }
 
 // (2) search by roomName
         List<Comment> commentListRoomName = new ArrayList<>();
-        if ("".equals(roomNameSearch)){
+        if ("".equals(roomNameSearch)) {
             commentListRoomName = commentListUserName;
         } else {
-            for (Comment room: commentListUserName ){
-                if ((room.getMeetingRoom().getRoomName().toLowerCase()).contains(roomNameSearch.toLowerCase())){
+            for (Comment room : commentListUserName) {
+                if ((room.getMeetingRoom().getRoomName().toLowerCase()).contains(roomNameSearch.toLowerCase())) {
                     commentListRoomName.add(room);
                 }
             }
         }
 // (3) search by status
         List<Comment> commentList = new ArrayList<>();
-        if (!statusSearch){
+        if (!statusSearch) {
             commentList = commentListRoomName;
         } else {
-            for (Comment room: commentListRoomName ){
-                if (statusSearch == room.isStatus()){
+            for (Comment room : commentListRoomName) {
+                if (statusSearch == room.isStatus()) {
                     commentList.add(room);
                 }
             }
@@ -99,20 +115,20 @@ public class CommentController {
 // (3) convert commentDTO
         List<CommentDTO> commentListDTO = new ArrayList<>();
         for (Comment commentLists : commentList) {
-                if (commentLists.getReplier() == null) {
-                    commentListDTO.add(new CommentDTO(commentLists.getIdComment(), commentLists.getSender().getFullName(), commentLists.getCommentTime(), "N/A",
-                            commentLists.getContentComment(), "N/A", commentLists.getErrorType().getErrorTypeName(),
-                            commentLists.getMeetingRoom().getRoomName(), commentLists.isStatus(),commentLists.isStatusView()));
-                } else {
-                    commentListDTO.add(new CommentDTO(commentLists.getIdComment(), commentLists.getSender().getFullName(),
-                            commentLists.getCommentTime(), commentLists.getContentReply(),
-                            commentLists.getContentComment(), commentLists.getReplier().getFullName(), commentLists.getErrorType()
-                            .getErrorTypeName(), commentLists.getMeetingRoom().getRoomName(), commentLists.isStatus(),commentLists.isStatusView()));
+            if (commentLists.getReplier() == null) {
+                commentListDTO.add(new CommentDTO(commentLists.getIdComment(), commentLists.getSender().getFullName(), commentLists.getCommentTime(), "N/A",
+                        commentLists.getContentComment(), "N/A", commentLists.getErrorType().getErrorTypeName(),
+                        commentLists.getMeetingRoom().getRoomName(), commentLists.isStatus(), commentLists.isStatusView()));
+            } else {
+                commentListDTO.add(new CommentDTO(commentLists.getIdComment(), commentLists.getSender().getFullName(),
+                        commentLists.getCommentTime(), commentLists.getContentReply(),
+                        commentLists.getContentComment(), commentLists.getReplier().getFullName(), commentLists.getErrorType()
+                        .getErrorTypeName(), commentLists.getMeetingRoom().getRoomName(), commentLists.isStatus(), commentLists.isStatusView()));
 
-                }
             }
+        }
         if (commentListDTO.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(commentListDTO,HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(commentListDTO, HttpStatus.OK);
     }
@@ -122,35 +138,44 @@ public class CommentController {
 
         Comment comment = commentService.findById(idComment);
         CommentDTO commentDTO;
-        if (comment.getReplier() == null) {
-            commentDTO = new CommentDTO(comment.getIdComment(), comment.getSender().getFullName(), comment.getCommentTime(), comment.getContentReply(),
-                    comment.getContentComment(), "N/A", comment.getErrorType().getErrorTypeName(),
-                    comment.getMeetingRoom().getRoomName(), comment.isStatus(),comment.isStatusView());
+        if (comment == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
-            commentDTO = new CommentDTO(comment.getIdComment(), comment.getSender().getFullName(), comment.getCommentTime(), comment.getContentReply(),
-                    comment.getContentComment(), comment.getReplier().getFullName(), comment.getErrorType().getErrorTypeName(),
-                    comment.getMeetingRoom().getRoomName(), comment.isStatus(), comment.isStatusView());
+            if (comment.getReplier() == null) {
+                commentDTO = new CommentDTO(comment.getIdComment(), comment.getSender().getFullName(), comment.getCommentTime(), comment.getContentReply(),
+                        comment.getContentComment(), "N/A", comment.getErrorType().getErrorTypeName(),
+                        comment.getMeetingRoom().getRoomName(), comment.isStatus(), comment.isStatusView());
+            } else {
+                commentDTO = new CommentDTO(comment.getIdComment(), comment.getSender().getFullName(), comment.getCommentTime(), comment.getContentReply(),
+                        comment.getContentComment(), comment.getReplier().getFullName(), comment.getErrorType().getErrorTypeName(),
+                        comment.getMeetingRoom().getRoomName(), comment.isStatus(), comment.isStatusView());
+            }
+            return new ResponseEntity<>(commentDTO, HttpStatus.OK);
         }
-        return new ResponseEntity<>(commentDTO, HttpStatus.OK);
     }
 
     @PutMapping("/comment/{idComment}")
-    public ResponseEntity<Void> addCommentHandle(@PathVariable Long idComment, @RequestBody CommentDTO commentDTO) {
+    public ResponseEntity<Void> CommentHandle(@PathVariable Long idComment, @RequestBody CommentDTO commentDTO) {
         Comment comment = commentService.findById(idComment);
         comment.setStatus(false);
-        comment.setReplier(userService.findById((long) 2));
+        comment.setCommentTime(String.valueOf(LocalDate.now()));
+        comment.setReplier(userService.findByUserName(commentDTO.getReplierName()));
+        System.err.println(commentDTO.getReplierName());
         comment.setContentReply(commentDTO.getContentReply());
         commentService.save(comment);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-//user
+    //user
     @PutMapping("/comment/detail/{idComment}")
     public ResponseEntity<Void> detailCommentHandle(@PathVariable Long idComment, @RequestBody CommentDTO commentDTO) {
         Comment comment = commentService.findById(idComment);
+        if (comment == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
         comment.setStatusView(true);
         commentService.save(comment);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);}
     }
 
     @DeleteMapping("/comment/delete/{id}")
@@ -161,21 +186,48 @@ public class CommentController {
     }
 
     @PostMapping("/comment/create")
-    public ResponseEntity<Void> addComment(@Validated @RequestBody CommentDTO commentDTO , BindingResult bindingResult) {
-        if (bindingResult.hasErrors()){
+    public ResponseEntity<Void> addComment(@Validated @RequestBody CommentDTO commentDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        else {
+        } else {
             Comment comment = new Comment();
             comment.setCommentTime(String.valueOf(LocalDate.now()));
             comment.setContentComment(commentDTO.getContentComment());
             comment.setStatus(true);
-            comment.setSender(userService.findById((long) 1));
+            comment.setSender(userService.findByUserName((commentDTO.getSenderName())));
             comment.setMeetingRoom(meetingRoomService.findByRoomName(commentDTO.getRoomName()));
             comment.setErrorType(errorTypeService.findByErrorTypeName(commentDTO.getErrorTypeName()));
             comment.setReplier(null);
             comment.setContentReply(null);
             commentService.save(comment);
-            return new ResponseEntity<>(HttpStatus.OK);}
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
+    @GetMapping("/comment/notification/{userName}")
+    public ResponseEntity<List<CommentDTO>> getListCommentUserName(@PathVariable String userName) {
+        List<CommentDTO> commentDTO = new ArrayList<>();
+        List<Comment> commentList = commentService.findAllBySender_UserName(userName);
+        if (commentList == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            if (commentList.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } else {
+                for (Comment commentLists : commentList) {
+                    if (commentLists.getReplier() == null) {
+                        commentDTO.add(new CommentDTO(commentLists.getIdComment(), commentLists.getSender().getFullName(), commentLists.getCommentTime(), "N/A",
+                                commentLists.getContentComment(), "N/A", commentLists.getErrorType().getErrorTypeName(),
+                                commentLists.getMeetingRoom().getRoomName(), commentLists.isStatus(), commentLists.isStatusView()));
+                    } else {
+                        commentDTO.add(new CommentDTO(commentLists.getIdComment(), commentLists.getSender().getFullName(),
+                                commentLists.getCommentTime(), commentLists.getContentReply(), commentLists.getContentComment(),
+                                commentLists.getReplier().getFullName(), commentLists.getErrorType().getErrorTypeName(),
+                                commentLists.getMeetingRoom().getRoomName(), commentLists.isStatus(), commentLists.isStatusView()));
+
+                    }
+                }
+                return new ResponseEntity<>(commentDTO, HttpStatus.OK);
+            }
+        }
     }
 }
