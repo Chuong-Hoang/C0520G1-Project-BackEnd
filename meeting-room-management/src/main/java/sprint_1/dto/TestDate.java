ae93d2e0120af16f1f6cd7409083845d93fcc117
Chuong-Hoang <hoangkvchuong@gmail.com>
2020-11-28 00:21:02 +0700

    hoangkvchuong commit khuya 28/11/2020

--- meeting-room-management/src/main/java/sprint_1/dto/TestDate.java
+++ meeting-room-management/src/main/java/sprint_1/dto/TestDate.java
@@ -7,8 +7,9 @@ import java.util.Date;
 public class TestDate {
 
     public static void main(String[] args) throws Exception {
-//        String dateStr1 = "2020-11-24T17:00:00.000Z"; // 1.test compareDates
-//        String dateStr2 = "2020-11-24T17:00:00.000Z";
+        // 1.test compareDates
+//        String dateStr1 = "24-11-2020";
+//        String dateStr2 = "23-11-2020";
 //        int result = compareDates(dateStr1, dateStr2);
 //        if (result>0){
 //            System.out.println("-->date1 is later than date2");
@@ -17,14 +18,16 @@ public class TestDate {
 //        } else {
 //            System.out.println("-->date1 is equals to date2");
 //        }
-        String endDate = "2020-11-26"; // 2.Test deviation-hours.
+
+        // 2.Test deviation-hours.
+        String endDate = "26-11-2020";
         long endTime = 11;
-        System.out.println("deviation-hours: " + getDiffTime(endDate, endTime));
+        System.out.println("deviation-hours: " + getDiffTime(endDate, endTime) + " hours");
     }
 
     public static long getDiffTime(String endDate, long endTime) throws ParseException {
+        Date enDate = new SimpleDateFormat("dd-MM-yyyy").parse(endDate);
         Date moment = new Date();
-        Date enDate = new SimpleDateFormat("yyyy-MM-dd").parse(endDate);
         long nowMillis = moment.getTime();
         long endTimeMillis = (long) (((endTime - 1) * 0.5 + 7) * 3600 * 1000 + enDate.getTime());
         long deviation = nowMillis - endTimeMillis;
@@ -33,12 +36,12 @@ public class TestDate {
     }
 
     public static int compareDates(String dateStr1, String dateStr2) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         Date date1 = sdf.parse(dateStr1);
         Date date2 = sdf.parse(dateStr2);
 
-        System.out.println("date1 : " + sdf.format(date1));
-        System.out.println("date2 : " + sdf.format(date2));
+        System.out.println("date_1: " + sdf.format(date1));
+        System.out.println("date_2: " + sdf.format(date2));
 
         if (date1.compareTo(date2) > 0) {
             return 1;
