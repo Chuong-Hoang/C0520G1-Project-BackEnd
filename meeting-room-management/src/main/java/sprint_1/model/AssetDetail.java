40fbcdfc2076a028502d0f7c1211dc6cf7bc8567
letoandn2192 <talaquanlaogia123@gmail.com>
2020-11-29 14:59:37 +0700

    asset detail update

--- meeting-room-management/src/main/java/sprint_1/model/AssetDetail.java
+++ meeting-room-management/src/main/java/sprint_1/model/AssetDetail.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 @Entity
 public class AssetDetail {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idAssetDetail;
 
     private String assetQuantity;
